12c0e33362b0b1f12bf75bf2deed980d2c6e0c4d
nicrocs <ngjelley@gmail.com>
2014-03-07 13:52:11 -0600

    initial commit of formToWizard

---
+++ js/formToWizard.js
@@ -0,0 +1,86 @@
+// Edited by Gavin Todes
+/* Origital created by jankoatwarpspeed.com - http://www.jankoatwarpspeed.com/post/2009/09/28/webform-wizard-jquery.aspx*/
+
+(function($) {
+    $.fn.formToWizard = function(options) {
+        options = $.extend({
+            submitButton : "Submit",
+            validationEnabled : false
+        }, options);
+
+        var element = this;
+
+        var steps = $(element).find("fieldset");
+        var count = steps.size();
+        var submitButtonName = "#" + options.submitButton;
+        $(submitButtonName).hide();
+
+        // 2
+        $(element).before("<ul id='steps'></ul>");
+
+        steps.each(function(i) {
+            $(this).wrap("<div id='step" + i + "'></div>");
+            $(this).append("<p id='step" + i + "commands'></p>");
+
+            // 2
+            var name = $(this).find("legend").html();
+            $("#steps").append("<li id='stepDesc" + i + "'>Step " + (i + 1) + "<span>" + name + "</span></li>");
+
+            if (i == 0) {
+                createNextButton(i);
+                selectStep(i);
+            }
+            else if (i == count - 1) {
+                $("#step" + i).hide();
+                createPrevButton(i);
+            }
+            else {
+                $("#step" + i).hide();
+                createPrevButton(i);
+                createNextButton(i);
+            }
+        });
+
+        function createPrevButton(i) {
+            var stepName = "step" + i;
+            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
+
+            $("#" + stepName + "Prev").bind("click", function(e) {
+                $("#" + stepName).hide();
+                $("#step" + (i - 1)).show();
+                $(submitButtonName).hide();
+                selectStep(i - 1);
+            });
+        }
+
+        function createNextButton(i) {
+            var stepName = "step" + i;
+            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
+
+            $("#" + stepName + "Next").bind("click", function(e) {
+              if (options.validationEnabled) {
+                  var stepIsValid = true;
+                  $("#" + stepName + " :input").each( function(index) {
+                    var xy = element.validate().element($(this));
+                    if(xy == undefined) xy = true;
+                       stepIsValid = xy && stepIsValid;
+                  });
+                  if (!stepIsValid) {
+                       return false;
+                  };
+              };
+              $("#" + stepName).hide();
+              $("#step" + (i + 1)).show();
+              if (i + 2 == count)
+                  $(submitButtonName).show();
+              selectStep(i + 1);
+            });
+        }
+
+        function selectStep(i) {
+            $("#steps li").removeClass("current");
+            $("#stepDesc" + i).addClass("current");
+        }
+
+    }
+})(jQuery); 
